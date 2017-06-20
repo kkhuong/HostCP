4c8c205f836b3143150db128387ef77cd06124c2
Kevin Khuong <kkhuong@ucsd.edu>
2017-06-20 08:26:39 -0700

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Install Dependencies
+# INSTALL DEPENDENCIES
 sudo apt-get update
 sudo apt-get install -y make cmake g++ ruby rake libcurl4-openssl-dev libssl-dev zlib1g-dev ruby-dev
 
-# Removed some pre-installed softwares
+# REMOVE ANY EXISTING NGINX
 sudo apt-get purge nginx nginx-full nginx-light nginx-naxsi nginx-common
 sudo rm -rf /etc/nginx
 
-# Ruby and Passenger + NGINX
+# INSTALL PASSENGER + NGINX
 wget http://acsweb.ucsd.edu/~kkhuong/passenger-5.1.5.tar.gz
 tar -xzvf passenger-5.1.5.tar.gz -C /opt
 rm passenger-5.1.5.tar.gz
@@ -22,8 +22,14 @@ echo "" >> /etc/bash.bashrc
 PATH=/opt/passenger-5.1.5/bin:$PATH
 export PATH
 
-# install rack
-gem install rack
-
-# figure out how to install passenger and insert code here
+gem install rack  # required by passenger
 passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx  --languages ruby,python,nodejs,meteor
+
+# INSTALL RVM
+\curl -sSL https://get.rvm.io | sudo bash -s -- --version latest
+
+# make admin user
+# add admin user to rvm group: sudo usermod -a -G rvm USER
+# have the user install every single rubies
+# potentially delete the user?
+# setup paths?
