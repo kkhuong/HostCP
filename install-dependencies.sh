2deee94b4d4a6d7fcdc6e423c125285822bc043f
Kevin Khuong <kkhuong@ucsd.edu>
2017-06-21 08:08:43 -0700

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-useradd -m -p $(openssl passwd -1 -salt SALT TEMP_PASSWORD) -s /bin/bash kkhuong
+sudo useradd -m -p $(openssl passwd -1 -salt SALT TEMP_PASSWORD) -s /bin/bash kkhuong
 sudo usermod -a -G sudo kkhuong
 #sudo echo "kkhuong ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.tmp
 sudo -u kkhuong bash << EOF
