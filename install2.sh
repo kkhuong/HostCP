f23dee925496843acc60cdee805beeb48c1d0845
Daniel <troioi@gmail.com>
2017-06-22 03:29:00 +0000

    Fixed rvm use issue

--- install2.sh
+++ install2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo usermod -a -G rvm `whoami`
+sudo usermod -a -G rvm root
+sudo usermod -a -G rvm kkhuong
+
 rvmsudo cleanup all
+
 
 # install some rubies and dependencies for Passenger here
 # ... we are commiting this change right now to see if we need to do something here
@@ -10,20 +13,15 @@ rvmsudo cleanup all
 if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi
 if sudo grep -q secure_path /etc/sudoers; then export rvmsudo_secure_path=1; fi
 
+
+
 rvm reinstall ruby-2.3.3
 
+
+# doing this because previous command often fail to finish before this
+echo "Now using rvm ruby..."
+sleep 45
 rvm --default use ruby-2.3.3
 
 gem install bundler --no-rdoc --no-ri
 gem install rack  # required by passenger
-
-# INSTALL PASSENGER + NGINX
-gem install passenger --no-rdoc --no-ri
-passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx --languages ruby,python,nodejs,meteor
-
-
-# make admin user
-# add admin user to rvm group: sudo usermod -a -G rvm USER
-# have the user install every single rubies
-# potentially delete the user?
-# setup paths?
--- install3.sh
+++ install3.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+# INSTALL PASSENGER + NGINX
+rvmsudo gem install passenger --no-rdoc --no-ri
+passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx --languages ruby,python,nodejs,meteor
+
+
+# make admin user
+# add admin user to rvm group: sudo usermod -a -G rvm USER
+# have the user install every single rubies
+# potentially delete the user?
+# setup paths?
 # install system dependencies: ruby, nginx, node, passenger, (pureftp), (dns_stuff?), (postgres), (mysql), (mongodb)
 
 sudo cp -r misc/skeleton/. /etc/skel/
