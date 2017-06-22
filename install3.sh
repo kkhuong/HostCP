cef685f41edeeba3dbe2bc994a6856c635e3f9d0
Kevin Khuong <kkhuong@ucsd.edu>
2017-06-22 02:39:33 -0700

    DEFAULT RUBY APP SECRET KEY BASE

--- install3.sh
+++ install3.sh
@@ -6,15 +6,14 @@
 rvmsudo gem install passenger --no-rdoc --no-ri
 passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx --languages ruby,python,nodejs,meteor
 
+# DEFAULT RUBY APP SECRET KEY BASE
+sudo echo "" >> /etc/bash.bashrc
+sudo echo "# DEFAULT RUBY APP SECRET_KEY_BASE" >> /etc/bash.bashrc
+sudo echo 'export SECRET_KEY_BASE=eae2d4b09d90966e214853ffeecdab45b024293e541a9de58b4e63f0f167bd42f26c324869bf5283034f2b399ace7fbab5eb434ec28c54bb1d7832cbac9d054c' >> /etc/bash.bashrc
+export SECRET_KEY_BASE=eae2d4b09d90966e214853ffeecdab45b024293e541a9de58b4e63f0f167bd42f26c324869bf5283034f2b399ace7fbab5eb434ec28c54bb1d7832cbac9d054c
 
-# make admin user
-# add admin user to rvm group: sudo usermod -a -G rvm USER
-# have the user install every single rubies
-# potentially delete the user?
-# setup paths?
 # install system dependencies: ruby, nginx, node, passenger, (pureftp), (dns_stuff?), (postgres), (mysql), (mongodb)
 
 sudo cp -r misc/skeleton/. /etc/skel/
 
-# replace nginx default files for server_ip:80, (or perhaps disable it), 
-
+# replace nginx default files for server_ip:80, (or perhaps disable it),
