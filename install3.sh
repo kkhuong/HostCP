b03c295eb063a07c6e027d7a4017eec4c926b866
Your Name <you@example.com>
2017-06-26 23:44:42 +0000

    Now configuring NGINX

--- install3.sh
+++ install3.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # INSTALL PASSENGER + NGINX
 rvmsudo gem install passenger --no-rdoc --no-ri
 passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx --languages ruby,python,nodejs,meteor
@@ -12,8 +10,13 @@ sudo echo "# DEFAULT RUBY APP SECRET_KEY_BASE" >> /etc/bash.bashrc
 sudo echo 'export SECRET_KEY_BASE=eae2d4b09d90966e214853ffeecdab45b024293e541a9de58b4e63f0f167bd42f26c324869bf5283034f2b399ace7fbab5eb434ec28c54bb1d7832cbac9d054c' >> /etc/bash.bashrc
 export SECRET_KEY_BASE=eae2d4b09d90966e214853ffeecdab45b024293e541a9de58b4e63f0f167bd42f26c324869bf5283034f2b399ace7fbab5eb434ec28c54bb1d7832cbac9d054c
 
-# install system dependencies: ruby, nginx, node, passenger, (pureftp), (dns_stuff?), (postgres), (mysql), (mongodb)
+# install system dependencies: ruby (done), nginx (done), node (done), passenger (done), (pureftp), (dns_stuff?), (postgres), mysql (done), (mongodb)
 
 sudo cp -r misc/skeleton/. /etc/skel/
 
-# replace nginx default files for server_ip:80, (or perhaps disable it),
+# replace nginx default config files
+sudo mkdir /opt/nginx/sites-available
+sudo ln -s /opt/nginx/sites-available /opt/nginx/sites-enabled
+sudo cp -f misc/conf/nginx.conf /opt/nginx/nginx.conf
+sudo kill $(cat /opt/nginx/logs/nginx.pid)  # here we restart nginx
+sudo /opt/nginx/sbin/nginx
